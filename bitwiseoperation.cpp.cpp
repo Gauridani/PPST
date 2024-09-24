156437f022d35483d02ef70e409238c19d252b2a
Gauridani <gauri.dani24@sanjivani.edu.in>
2024-09-24 09:12:28 +0530

    Add files via upload

---
+++ bitwiseoperation.cpp.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a=2, b=4;
+    //Binary AND operator
+    cout<<" a&b is"<<(a&b)<<endl;
+    
+    //Binary OR operator
+    cout<<" a/b is"<<(a/b)<<endl;
+    
+    //Binary XOR operator
+    cout<<" a^b is"<<(a<<1)<<endl;
+    
+    //Left shift operator
+    cout<<" a>>1 is"<<(a<<1)<<endl;
+    
+    //Right shift operator
+    cout<<" a>>1 is"<<(a>>1)<<endl;
+    
+    //One's complement operator
+    cout<<" ~(a) is"<< ~(a)<<endl;
+    
+    return 0;
+}
