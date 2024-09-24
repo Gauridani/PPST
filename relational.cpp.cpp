ba6905393486f1fffcca21cf28bdfaab7620a077
Gauridani <gauri.dani24@sanjivani.edu.in>
2024-09-24 09:12:05 +0530

    Add files via upload

---
+++ relational.cpp.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int a=6, b=4
+  //Equal to operator
+  cout<<"a==b is"<<(a==b)<<endl;
+  
+  //Greater than operator
+  cout<<"a>b is"<<(a>b)<<endl;
+  
+  //Greater than or equal to
+  cout<<"a>=b is"<<(a>=b)endl;
+  
+  //Lesser than operator
+  cout<<"a<b is"<<(a<b)<<endl;
+  
+  //Lesser than or equal to operator
+  cout<<"a<=b is"<<(a<=b)<<endl;
+  
+  //True
+  cout<<"a!=b is"<<(a!=b)endl;
+  
+  return 0;
+} 
+  
